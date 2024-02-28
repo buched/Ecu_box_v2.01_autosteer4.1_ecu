cb9bb6796d80c8fe8d8d46d9b924ba95e39cff34
buched <32975584+buched@users.noreply.github.com>
2024-02-28 19:30:13 +0100

    Canbus enable (#2)
    * Update Autosteer.ino
    
    add can engage possibility
    
    * Add files via upload
    
    * Update Autosteer.ino
    
    * Update CAN_ALL_Brands.ino
    
    * Update Autosteer.ino

---
+++ CAN_ALL_Brands.ino
@@ -0,0 +1,28 @@
+void CAN_setup (void) {
+
+//V_Bus is CAN-3 and is the Steering BUS
+  V_Bus.begin();
+  V_Bus.setBaudRate(250000);
+  //V_Bus.enableFIFO();
+  //V_Bus.setFIFOFilter(REJECT_ALL);
+  //V_Bus.setFIFOFilter(0, 0x18FF1A03, EXT);  //Claas Curve Data & Valve State Message
+  delay(500);
+} //End CAN SETUP
+
+//---Receive K_Bus message
+void VBus_Receive()
+{
+CAN_message_t msgi;
+      if ( V_Bus.read(msgi) ) 
+          {
+            if (msgi.id == 0x18FF1A03)
+              {
+                if ((msgi.buf[2])== 0x15)
+                  {
+                    Time = millis();
+                    engageCAN = 1;
+                    relayTime = ((millis() + 1000));
+                   }
+               }
+          }
+}
