5832b3909539bfce9523a385696f2e875b7d168d
buched <32975584+buched@users.noreply.github.com>
2024-03-10 19:54:25 +0100

    Update CAN_ALL_Brands.ino

--- CAN_ALL_Brands.ino
+++ CAN_ALL_Brands.ino
@@ -3,9 +3,9 @@ void CAN_setup (void) {
 //V_Bus is CAN-3 and is the Steering BUS
   V_Bus.begin();
   V_Bus.setBaudRate(250000);
-  //V_Bus.enableFIFO();
-  //V_Bus.setFIFOFilter(REJECT_ALL);
-  //V_Bus.setFIFOFilter(0, 0x18FF1A03, EXT);  //Claas Curve Data & Valve State Message
+  V_Bus.enableFIFO();
+  V_Bus.setFIFOFilter(REJECT_ALL);
+  V_Bus.setFIFOFilter(0, 0x18FF1A03, EXT);  //Valve State Message
   delay(500);
 } //End CAN SETUP
 
